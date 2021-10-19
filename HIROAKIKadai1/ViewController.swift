2db4680546b9d51756047ad07a2330b013a0a7d3
hirokaissh <hirokai.saito1014@gmail.com>
2021-10-20 07:30:39 +0900

    LabelとtextFiledの宣言

--- HIROAKIKadai1/ViewController.swift
+++ HIROAKIKadai1/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var TextField1: UITextField!
+    @IBOutlet weak var TextField2: UITextField!
+    @IBOutlet weak var TextField3: UITextField!
+    @IBOutlet weak var TextField4: UITextField!
+    @IBOutlet weak var TextField5: UITextField!
+    @IBOutlet weak var TotalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
