67cd0cdf5bc54ace84ebbda872f9319e0afbdcf1
hirokaissh <hirokai.saito1014@gmail.com>
2021-10-24 11:41:16 +0900

    アウトレットの小文字に変更

--- HIROAKIKadai1/ViewController.swift
+++ HIROAKIKadai1/ViewController.swift
@@ -9,31 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet private weak var TextField1: UITextField!
-    @IBOutlet private weak var TextField2: UITextField!
-    @IBOutlet private weak var TextField3: UITextField!
-    @IBOutlet private weak var TextField4: UITextField!
-    @IBOutlet private weak var TextField5: UITextField!
-    @IBOutlet private weak var TotalLabel: UILabel!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var textField3: UITextField!
+    @IBOutlet private weak var textField4: UITextField!
+    @IBOutlet private weak var textField5: UITextField!
+    @IBOutlet private weak var totalLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        TextField1.keyboardType = .decimalPad
-        TextField2.keyboardType = .decimalPad
-        TextField3.keyboardType = .decimalPad
-        TextField4.keyboardType = .decimalPad
-        TextField5.keyboardType = .decimalPad
+        textField1.keyboardType = .decimalPad
+        textField2.keyboardType = .decimalPad
+        textField3.keyboardType = .decimalPad
+        textField4.keyboardType = .decimalPad
+        textField5.keyboardType = .decimalPad
         // Do any additional setup after loading the view.
     }
     
     @IBAction func CalculationButton(_ sender: Any) {
         
         let Totalnum:Double
-        let num1 = (TextField1.text! as NSString).doubleValue
-        let num2 = (TextField2.text! as NSString).doubleValue
-        let num3 = (TextField3.text! as NSString).doubleValue
-        let num4 = (TextField4.text! as NSString).doubleValue
-        let num5 = (TextField5.text! as NSString).doubleValue
+        let num1 = (textField1.text! as NSString).doubleValue
+        let num2 = (textField2.text! as NSString).doubleValue
+        let num3 = (textField3.text! as NSString).doubleValue
+        let num4 = (textField4.text! as NSString).doubleValue
+        let num5 = (textField5.text! as NSString).doubleValue
         
         Totalnum = num1 + num2 + num3 + num4 + num5
         
